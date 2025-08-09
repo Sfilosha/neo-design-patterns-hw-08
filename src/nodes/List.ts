4d910c9b09742d14102ee49d47b1c1bb0624ab38
Sfilosha <sfilosha@gmail.com>
2025-08-09 14:12:49 +0300

    complete task

---
+++ src/nodes/List.ts
@@ -0,0 +1,21 @@
+import { DocNode } from "../interfaces/DocNode";
+import { DocRenderer } from "../interfaces/DocRenderer";
+import { RenderEventPublisher } from "../RenderEventPublisher";
+
+export class List implements DocNode {
+  constructor(private items: string[], private renderer: DocRenderer) {}
+  render(): string {
+    const start = performance.now();
+    const result = this.renderer.renderList(this.items);
+    const end = performance.now();
+
+    RenderEventPublisher.notify({
+      type: 'List',
+      content: result,
+      items: this.items,
+      renderTime: Math.round(end - start)
+    });
+
+    return result;
+  }
+}
---
+++ src/nodes/Paragraph.ts
@@ -0,0 +1,20 @@
+import { DocNode } from "../interfaces/DocNode";
+import { DocRenderer } from "../interfaces/DocRenderer";
+import { RenderEventPublisher } from "../RenderEventPublisher";
+
+export class Paragraph implements DocNode {
+  constructor(private text: string, private renderer: DocRenderer) {}
+  render(): string {
+    const start = performance.now();
+    const result = this.renderer.renderParagraph(this.text);
+    const end = performance.now();
+
+    RenderEventPublisher.notify({
+      type: 'Paragraph',
+      content: this.text,
+      renderTime: Math.round(end - start)
+    });
+
+    return result;
+  }
+}
